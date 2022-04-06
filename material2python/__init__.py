326178fab212e8653eac1a18f679fe82699eacd1
DreamSpoon <flaskofwater@gmail.com>
2022-04-06 16:00:17 -0400

    Output current material shader nodes to a Python text-block.

---
+++ material2python/__init__.py
@@ -0,0 +1,67 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 3
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+bl_info = {
+    "name": "Material 2 Python",
+    "description": "Convert material nodes to Python text-block.",
+    "author": "Dave",
+    "version": (0, 0, 1),
+    "blender": (2, 80, 0),
+    "location": "Material Shader Nodes",
+    "category": "Material Python",
+}
+
+import bpy as boop
+from .mat2py import Mat2Python
+
+if boop.app.version < (2,80,0):
+    Region = "TOOLS"
+else:
+    Region = "UI"
+
+class M2P_MaterialToPython(boop.types.Panel):
+    bl_idname = "NODE_PT_material2python"
+    bl_label = "Mat2Python"
+    bl_space_type = "NODE_EDITOR"
+    bl_region_type = Region
+    bl_category = "Mat2Python"
+
+    def draw(self, fun):
+        layout = self.layout
+        box = layout.box()
+        box.label(text="Great Stuff")
+        box.operator("major.awesome")
+        box.prop(fun.scene, "M2P_NumSpacePad")
+        box.prop(fun.scene, "M2P_KeepLinks")
+
+def register():
+    boop.utils.register_class(Mat2Python)
+    boop.utils.register_class(M2P_MaterialToPython)
+
+    bts = boop.types.Scene
+    bp = boop.props
+    bts.M2P_NumSpacePad = bp.IntProperty(name="Num Space Pad", description="Number of spaces to prepend to each " +
+        "line of code output in text-block", default=4, min=0)
+    bts.M2P_KeepLinks = bp.BoolProperty(name="Keep Links", description="Add created links to a list variable", default=True)
+
+def unregister():
+    boop.utils.unregister_class(Mat2Python)
+    boop.utils.unregister_class(M2P_MaterialToPython)
+
+if __name__ == "__main__":
+    register()
---
+++ material2python/mat2py.py
@@ -0,0 +1,83 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 3
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+import bpy as boop
+
+M2P_TEXT_NAME = "m2pText"
+
+class Mat2Python(boop.types.Operator):
+    """Make Python text-block from current Material node tree"""
+    bl_idname = "major.awesome"
+    bl_label = "Material to Python"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, fun):
+        s = fun.space_data
+        if s.type == 'NODE_EDITOR' and s.node_tree != None and \
+            s.tree_type in ("CompositorNodeTree", "ShaderNodeTree", "TextureNodeTree"):
+            return True
+        return False
+
+    def execute(self, fun):
+        self.do_it(fun, fun.scene.M2P_NumSpacePad, fun.scene.M2P_KeepLinks)
+        return {'FINISHED'}
+
+    @classmethod
+    def do_it(cls, fun, space_pad, keep_links):
+        if isinstance(space_pad, int):
+            pres = " " * space_pad
+        elif isinstance(space_pad, str):
+            pres = space_pad
+        else:
+            pres = ""
+
+        mat = fun.space_data
+
+        m2p_text = boop.data.texts.new(M2P_TEXT_NAME)
+
+        m2p_text.write(pres + "# initialize variables\n")
+        m2p_text.write(pres + "new_nodes = {}\n")
+        if keep_links:
+            m2p_text.write(pres + "new_links = []\n")
+        m2p_text.write(pres + "tree_nodes = material.node_tree.nodes\n\n")
+        m2p_text.write(pres + "# material shader nodes\n")
+        for tree_node in mat.node_tree.nodes:
+            m2p_text.write(pres + "node = tree_nodes.new(type=\"" + tree_node.bl_idname + "\")\n")
+            m2p_text.write(pres + "node.location = (" + str(round(tree_node.location.x, 3)) + ", " + str(round(tree_node.location.y, 3)) + ")\n")
+            m2p_text.write(pres + "new_nodes[\"" + tree_node.name + "\"] = node\n\n")
+
+        m2p_text.write(pres + "# links between nodes\n")
+        for tree_link in mat.node_tree.links:
+            m2p_text.write(pres + "link = tree_links.new(new_nodes[\"" + tree_link.from_socket.node.name + "\"].outputs[" + str(cls.get_output_num_for_link(tree_link)) + "], new_nodes[\"" + tree_link.to_socket.node.name + "\"].inputs[" + str(cls.get_input_num_for_link(tree_link)) + "])\n")
+            if keep_links:
+                m2p_text.write(pres + "new_links.append(link)\n\n")
+
+    @classmethod
+    def get_input_num_for_link(cls, tr_link):
+        for c in range(0, len(tr_link.to_socket.node.inputs)):
+            if tr_link.to_socket.node.inputs[c] == tr_link.to_socket:
+                return c
+        return None
+
+    @classmethod
+    def get_output_num_for_link(cls, tr_link):
+        for c in range(0, len(tr_link.from_socket.node.outputs)):
+            if tr_link.from_socket.node.outputs[c] == tr_link.from_socket:
+                return c
+        return None
